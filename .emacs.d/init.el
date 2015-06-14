edc28e300a4343a03150f7d0ea778550bc7be7f3
jkr <joakim.karlsson@lundalogik.se>
2015-06-14 18:39:57 +0200

    [emacs]: commiting last changes from emacs week

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,7 +15,13 @@
 (use-package powerline-evil
   :ensure powerline-evil
   :demand powerline-evil
-  :init (powerline-default-theme))
+  :init (powerline-evil-vim-color-theme))
+
+(defun run-python-tests ()
+  "Run python tests using nosetests"
+  (interactive)
+  (save-some-buffers 'no-confirm)
+  (compile "nosetests"))
 
 (use-package evil-leader
   :commands (evil-leader-mode)
@@ -26,11 +32,28 @@
   (progn
     (evil-leader/set-leader "<SPC>")
     (evil-leader/set-key "b" 'ibuffer)
+
     (evil-leader/set-key "<SPC>" 'other-window)
     (evil-leader/set-key "p" 'helm-projectile)
     (evil-leader/set-key "sp" 'helm-projectile-switch-project)
     (evil-leader/set-key "f" 'toggle-frame-fullscreen)
+    (evil-leader/set-key "ra" 'run-python-tests)
     )
+  )
+
+(use-package magit
+  :ensure t
+  :config
+  (progn
+    (setq magit-last-seen-setup-instructions "1.4.0")
+    )
+  )
+
+(use-package org
+  :ensure t
+  )
+(use-package evil-org
+  :ensure t
   )
 
 ;; Spaces, not tabs
@@ -38,7 +61,7 @@
 
 ;; Remove GUI stuff
 (tool-bar-mode -1)
-(scroll-bar-mode -1)
+;(scroll-bar-mode -1)
 (menu-bar-mode -1)
 
 ;;dark colortheme
@@ -64,7 +87,7 @@
   :config
   (progn
     (projectile-global-mode)
-    (setq projectile-enable-caching t)
+    (add-to-list 'projectile-globally-ignored-directories "venv" "node_modules")
     )
   )
 
@@ -80,8 +103,13 @@
 (setq backup-inhibited t)
 (setq auto-save-default nil)
 
+;; Show matching parens
+(show-paren-mode 1)
+
 (defun my-python-mode-hook ()
-  (linum-mode 1))
+  (linum-mode 1)
+  (py-electric-backspace 1)
+  (normal-erase-is-backspace-mode 1))
 (add-hook 'python-mode-hook 'my-python-mode-hook)
 
 (defun my-js-mode-hook ()
@@ -133,7 +161,7 @@
   )
 
 (use-package key-chord
-  :ensure key-chord
+ :ensure key-chord
   :demand key-chord
   :init (key-chord-mode 1)
   :config
@@ -141,18 +169,3 @@
     (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
     )
   )
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
